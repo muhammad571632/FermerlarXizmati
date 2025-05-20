bd692f5bd9310baa4a806f19d251d95748e04af5
Samandar-dev-hack <soatmurotovsamandar2327@gmail.com>
2025-05-19 23:03:10 -0700

    User class qoshdim

---
+++ src/entity/User.java
@@ -0,0 +1,57 @@
+package entity;
+
+public class User {
+    private String name;
+    private String surname;
+    private String password;
+    private String email;
+    public User(){}
+    public User(String name, String surname, String password, String email) {
+        this.name = name;
+        this.surname = surname;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
