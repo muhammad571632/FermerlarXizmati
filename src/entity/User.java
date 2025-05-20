c442f912ffc63af4b53bb5351b9825c60ae108a0
Samandar-dev-hack <soatmurotovsamandar2327@gmail.com>
2025-05-20 01:13:46 -0700

    User classlarni uzgartirdim

--- src/entity/User.java
+++ src/entity/User.java
@@ -1,16 +1,77 @@
 package entity;
 
+import enums.Role;
+
+import java.time.LocalTime;
+import java.util.UUID;
+
 public class User {
+    private final  String userId = UUID.randomUUID().toString();
     private String name;
     private String surname;
     private String password;
     private String email;
+    private Role role;
+    private LocalTime ben;
+    private LocalTime end;
+    private boolean IsConfer;
+    private Integer code;
     public User(){}
-    public User(String name, String surname, String password, String email) {
+
+    public User(String name, String surname, String password, String email, Role role, LocalTime ben, LocalTime end, boolean isConfer, Integer code) {
         this.name = name;
         this.surname = surname;
         this.password = password;
         this.email = email;
+        this.role = role;
+        this.ben = ben;
+        this.end = end;
+        IsConfer = isConfer;
+        this.code = code;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public LocalTime getBen() {
+        return ben;
+    }
+
+    public void setBen(LocalTime ben) {
+        this.ben = ben;
+    }
+
+    public LocalTime getEnd() {
+        return end;
+    }
+
+    public void setEnd(LocalTime end) {
+        this.end = end;
+    }
+
+    public boolean isConfer() {
+        return IsConfer;
+    }
+
+    public void setConfer(boolean confer) {
+        IsConfer = confer;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
     }
 
     public String getName() {
@@ -48,10 +109,16 @@ public class User {
     @Override
     public String toString() {
         return "User{" +
-                "name='" + name + '\'' +
+                "userId='" + userId + '\'' +
+                ", name='" + name + '\'' +
                 ", surname='" + surname + '\'' +
                 ", password='" + password + '\'' +
                 ", email='" + email + '\'' +
+                ", role=" + role +
+                ", ben=" + ben +
+                ", end=" + end +
+                ", IsConfer=" + IsConfer +
+                ", code=" + code +
                 '}';
     }
 }
